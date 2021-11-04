a39f0e24471efd28e6481b36557cb6a396b91332
oumsamon <oumsamon@gmail.com>
2021-11-04 13:59:39 -0500

    Express Params

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const express = require("express")
 const app = express()
 
+// Routing
+
+app.get('/', (request, response) => {
+    response.send('Hello World')
+  })
+
+  
+// Params in Express  
+app.get("/:food", (req, res) => {
+  res.send(`I like ${req.params.food}`);
+});
+
+
 app.listen(4000, () => {
   console.log("app listening on port 4000")
 })
