348021d2de8cd7cef70123e424e35b34a2c3cc86
oumsamon <oumsamon@gmail.com>
2021-11-04 14:10:49 -0500

    install hbs and create hbs dir

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 const express = require("express")
 const app = express()
 
+//using hbs in express
+
+app.set('view engine', 'hbs')
+
 // Routing
 
 app.get('/', (request, response) => {
     response.send('Hello World')
   })
 
-  
+
 // Params in Express  
 app.get("/:food", (req, res) => {
   res.send(`I like ${req.params.food}`);
 });
+
+//using hbs below to match files in views
+app.get('/:name', function(req, res) {
+    res.render('index', { name: req.params.name })
+  })
 
 
 app.listen(4000, () => {
